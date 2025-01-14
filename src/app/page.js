6a4fc52ddb5d6bfb781bd50b28d5a87c1b844470
Omar Faruk <omarbg.bd@gmail.com>
2025-01-14 10:38:41 +0600

    most populer section

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Hero from "@/app/components/Hero";
 import CustomizableSection from "@/app/components/CustomizableSection";
 import ExclusiveInsights from "@/app/components/ExclusiveInsights";
+import MostPopular from "@/app/components/MostPopular";
 
 
 
@@ -9,21 +10,21 @@ export default function Home () {
 const trendingNews = [
   {
     image:
-      "https://images.pexels.com/photos/206557/pexels-photo-206557.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      "https://images.pexels.com/photos/3184418/pexels-photo-3184418.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     title: "5 Tips to Save Money Booking Your Next Hotel Room",
     category: "Europe",
     description: "This is a wider card with supporting text below...",
   },
   {
     image:
-      "https://images.pexels.com/photos/206557/pexels-photo-206557.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      "https://images.pexels.com/photos/210186/pexels-photo-210186.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     title: "How to Improve Your Home Interior Design",
     category: "Interior",
     description: "Discover simple tips to enhance your living space.",
   },
   {
     image:
-      "https://images.pexels.com/photos/206557/pexels-photo-206557.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      "https://images.pexels.com/photos/325185/pexels-photo-325185.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     title: "Top 5 Destinations for Budget Travelers",
     category: "Travel",
     description: "Explore beautiful destinations without breaking the bank.",
@@ -33,51 +34,49 @@ const trendingNews = [
 const editorsPicks = [
   {
     image:
-      "https://images.pexels.com/photos/206557/pexels-photo-206557.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      "https://images.pexels.com/photos/374052/pexels-photo-374052.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     title: "Top 10 Gadgets of 2025",
     category: "Tech",
     description: "Stay updated with the latest tech trends.",
   },
   {
     image:
-      "https://images.pexels.com/photos/206557/pexels-photo-206557.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      "https://images.pexels.com/photos/459653/pexels-photo-459653.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     title: "Best Travel Destinations for 2025",
     category: "Travel",
     description: "Plan your next adventure with these destinations.",
   },
   {
     image:
-      "https://images.pexels.com/photos/206557/pexels-photo-206557.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      "https://images.pexels.com/photos/1571460/pexels-photo-1571460.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     title: "Top Interior Design Trends of 2025",
     category: "Interior",
     description: "Elevate your living space with these trends.",
   },
 ];
 
-  
-
 const articles = [
   {
     image:
-      "https://images.pexels.com/photos/206557/pexels-photo-206557.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      "https://images.pexels.com/photos/1236701/pexels-photo-1236701.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     title: "Exploring the Future of AI",
     category: "Tech",
   },
   {
     image:
-      "https://images.pexels.com/photos/206557/pexels-photo-206557.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      "https://images.pexels.com/photos/1486222/pexels-photo-1486222.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     title: "Top Travel Destinations for 2025",
     category: "Travel",
   },
   {
     image:
-      "https://images.pexels.com/photos/206557/pexels-photo-206557.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      "https://images.pexels.com/photos/1643383/pexels-photo-1643383.jpeg?auto=compress&cs=tinysrgb&w=800",
     title: "Design Trends for Modern Homes",
     category: "Interior",
   },
   {
     image:
-      "https://images.pexels.com/photos/206557/pexels-photo-206557.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      "https://images.pexels.com/photos/237272/pexels-photo-237272.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     title: "The Rise of Electric Vehicles",
     category: "Auto",
   },
@@ -102,6 +101,9 @@ const articles = [
         sectionTitle="Exclusive Insights"
         articles={articles}
       />
+
+      <CustomizableSection sectionTitle="Top Stories" articles={editorsPicks} />
+      <MostPopular sectionTitle="Most Popular Articles" />
     </>
   );
 }
